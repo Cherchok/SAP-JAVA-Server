4515b5d417b920376441d214280470868906a0cb
cherchok <viktor.yurzanov@gmail.com>
2019-05-14 14:40:23 +0300

    add Resource Manager and setup file to set URL

--- src/main/java/main/StartServer.java
+++ src/main/java/main/StartServer.java
@@ -1,5 +1,6 @@
 package main;
 
+import connection.ResourceManager;
 import ws.WebDataImpl;
 
 import javax.xml.ws.Endpoint;
@@ -7,7 +8,25 @@ import javax.xml.ws.Endpoint;
 public class StartServer {
 
     public static void main(String[] args) {
-        Endpoint.publish("http://localhost:8082/ws/webdata", new WebDataImpl());
+        StringBuilder sb = new StringBuilder();
+        String address;
+        try {
+            ResourceManager rm = new ResourceManager();
+            for (String k : rm.getRb().keySet()) {
+                sb.append(rm.getValues(k));
+            }
+            address = String.valueOf(sb);
+        } catch (ExceptionInInitializerError exception) {
+            address = "http://localhost:8082/ws/webdata";
+            System.out.println(" 'setup.properties' file missing. Default URL was loaded!!!");
+        }
+
+        try {
+            Endpoint.publish(address, new WebDataImpl());
+        } catch (Exception e) {
+            Endpoint.publish("http://localhost:8082/ws/webdata", new WebDataImpl());
+            System.out.println("Wrong URL. Default URL was loaded!!!");
+        }
         System.out.println("Done");
     }
 }
